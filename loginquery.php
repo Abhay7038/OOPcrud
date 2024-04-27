f2fec9a8230cc4f6808d7643d417aee756703b57
Abhay Thakur <thakurabhay0323@gmail.com>
2024-04-28 01:42:07 +0530

    little change and Table.php created

--- loginquery.php
+++ loginquery.php
@@ -1,5 +1,6 @@
 <?php
 include("class.php");
+query::DBconnect();
 
 // Validate and sanitize user input
 $name = isset($_POST['name']) ? $_POST['name'] : '';
@@ -7,14 +8,13 @@ $password = isset($_POST['password']) ? $_POST['password'] : '';
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     //echo "ho";
     // Assuming 'selectAll' method returns a result, store it in a variable
-    query::DBconnect();
     $data = array();
     $data[] = "Name='$name'";
     $data[] .= "Password='$password'";
     $where_clause = implode(' AND ', $data);
 
     $alldata=query::selectAll('User', $where_clause);
-
+    header("location:table.php");
     // echo "hi";
     while($user = $alldata->fetch_all()){
         echo "<pre>";
