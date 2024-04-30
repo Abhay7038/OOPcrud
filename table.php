cdb8c3b3026344645f0026142886ee2036a0ee5b
Abhay Thakur <thakurabhay0323@gmail.com>
2024-04-30 15:12:49 +0530

    change in table and query

--- table.php
+++ table.php
@@ -6,15 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
 </head>
 <body>
-    <div
-        class="table-responsive"
-    >
-        <table
-            class="table table-striped table-hover table-borderless table-primary align-middle"
-        >
+    <div class="table-responsive">
+        <table class="table table-striped table-hover table-borderless table-primary align-middle">
             <thead class="table-light">
                 <tr>
                     <th>ID</th>
@@ -26,42 +21,24 @@
                 </tr>
             </thead>
             <tbody class="table-group-divider">
-                <?php foreach($data as $data){ ?>
-                <tr
-                    class="table-primary"
-                >
+                <?php foreach($userdata as $data){ ?>
+                <tr class="table-primary">
                     <td scope="row"><?php echo $data['ID']; ?></td>
                     <td><?php echo $data['Name']; ?></td>
                     <td><?php echo $data['Email']; ?></td>
                     <td><img src="uploads/<?php echo $data['File']; ?>" alt="" srcset="" style="width:50px;"></td>
                     <td><?php echo $data['Password']; ?></td>
                     <td>
-                        <a
-                        name=""
-                        id=""
-                        class="btn btn-primary"
-                        href="update.php?id=<?php echo $data['ID']; ?>"
-                        role="button"
-                        >Edit</a
-                        >
-                        <a
-                        name=""
-                        id=""
-                        class="btn btn-primary"
-                        href="?del=<?php echo $data['ID']; ?>"
-                        role="button"
-                        >Delete</a
-                        >
-                </td>
+                        <a name="" id="" class="btn btn-primary" href="update.php?id=<?php echo $data['ID']; ?>" role="button">Edit</a>
+                        <a name="" id="" class="btn btn-primary" href="?del=<?php echo $data['ID']; ?>" role="button">Delete</a>
+                    </td>
                 </tr>
                 <?php }?>
-                </tbody>
+            </tbody>
             <tfoot>
-                
             </tfoot>
         </table>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 </body>
 </html>
--- tablequery.php
+++ tablequery.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 include("class.php");
 query::DBconnect();
-$result=query::selectAll('User','1');
-if($result->num_rows>0){
-    while($data[] = $result->fetch_assoc()) {
-         $data; // Append each row to the $userdata array
-        
+$result = query::selectAll('User', '1');
+$userdata = array(); // Initialize an array to store fetched data
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $userdata[] = $row; // Append each row to the $userdata array
     }
 }
 ?>
